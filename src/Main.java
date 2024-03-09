32bbef19686a63dfcc0bedb1e785f0e83395c263
Rajath12e <38193362+Rajath12e@users.noreply.github.com>
2024-03-09 14:56:08 -0500

    Java practice with code

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args)
+    {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of the rows ");
+
+        int Rows =sc.nextInt();
+        System.out.println("Enter the number of column");
+        int column=sc.nextInt();
+
+        for (int i=1;i<Rows;i++){
+            for(int j=1;j<=column;j++)
+            {
+
+            System.out.print("*  ");
+        }
+
+            System.out.println("");
+
     }
+}
 }
